16bcf46b2fd51090ab73c63e35e12c651f7c457f
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2023-10-19 23:23:15 +0530

    Mobile Sidebar
    Designed Responsive Sidebar

--- src/Components/Header2/Header2.jsx
+++ src/Components/Header2/Header2.jsx
@@ -1,14 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header2.css'
 
 const Header2 = () => {
+  const [openHeader, setHeader] = useState(false);
+  const [openShop, setShop] = useState(false);
+  const [openPartner, setPartner] = useState(false);
+  const [login, setlogin] = useState(false);
+
   return (
     <div className='Header-Mobile'>
-        <div className="content-header">
-            <img src="assets/bars-3-horizontal.svg" alt="horizontal-bar" />
-            <img src="assets/Acima.svg" alt="acima-logo" />
-            <img src="assets/user.svg" alt="user-img" />
+      <div className="content-header">
+        <img src="assets/bars-3-horizontal.svg" alt="horizontal-bar" onClick={() => setHeader(!openHeader)} />
+        <img src="assets/Acima.svg" alt="acima-logo" />
+        <img src="assets/user.svg" alt="user-img" />
+      </div>
+
+      <div className={`side-header ${openHeader ? 'active-mobile-header' : ''}`}>
+        <div className='mob-header-content-1'>
+          <img className='mobile-header-logo' src="assets/Acima.svg" alt="acima-logo" />
         </div>
+        <div className="mob-header-content-2">
+          <div>
+            How It Works
+          </div>
+          <div>
+            <div className={`way-shop-head ${openShop ? 'open' : 'hide'}`} onClick={() => setShop(!openShop)}>
+              Ways To Shop
+              <img src="assets/Chevron-Down.svg" alt="arrow-down" />
+            </div>
+            <div className={`wayshop-inside-content ${openShop ? 'open' : 'hide'}`}>
+              <span>Overview</span>
+              <span>Shop NearBy Stores</span>
+              <span>Shop Online Stores</span>
+              <span>Get the App</span>
+            </div>
+          </div>
+          {/* Partner Started  */}
+
+          <div>
+            <div className={`partner-head ${openPartner ? 'open' : 'hide'}`} onClick={()=> setPartner(!openPartner)}>
+              Partners
+              <img src="assets/Chevron-Down.svg" alt="arrow-down" />
+            </div>
+            <div className={`partners-content ${openPartner ? 'open' : 'hide'}`}>
+              <span>Shop Online Stores</span>
+              <span>Get the App</span>
+            </div>  
+          </div>
+        </div>
+        <div className='mob-header-content-3'>
+          <span>About Us</span>
+          <span>News</span>
+          <span>Support/FAQ</span>
+        </div>
+
+        <div className='mob-header-content-2'>
+          <div>
+            <div className={`partner-head ${login ? 'open' : 'hide'}`} onClick={()=> setlogin(!login)}>
+              Login
+              <img src="assets/Chevron-Down.svg" alt="arrow-down" />
+            </div>
+            <div className={`partners-content ${login ? 'open' : 'hide'}`}>
+              <span>Customer Login</span>
+              <span>Partner Login</span>
+            </div>  
+          </div>
+        </div>
+
+        <div className="mob-header-content-4">
+          <a href="" className='app-btn'>Get the App</a>
+        </div>
+        
+      </div>
     </div>
   )
 }

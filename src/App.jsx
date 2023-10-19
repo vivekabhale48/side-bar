644500a344a2d89700ac5de2719050bc11bdc033
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2023-10-19 23:44:11 +0530

    Added Ashley header logo
    Added Header 3 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './Components/Header1/Header'
+import Header3 from './Components/Header3/Header3'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
     <>
       {/* <h1>React app</h1> */}
       <Header></Header>
+      <Header3></Header3>
     </>
   )
 }
---
+++ src/Components/Header3/Header3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header3.css';
+
+const Header3 = () => {
+  return (
+    <div className='ashley-header'>
+        <img src="assets/Ashley-logo.svg" alt="" />
+    </div>
+  )
+}
+
+export default Header3

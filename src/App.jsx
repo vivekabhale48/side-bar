5c95d760367c8d739a5487bd38aecfd9b19804be
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2023-10-20 00:02:53 +0530

    Cover Image
    Cover image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './Components/Header1/Header'
 import Header3 from './Components/Header3/Header3'
+import MainCover from './Components/Main-Cover/MainCover'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
       {/* <h1>React app</h1> */}
       <Header></Header>
       <Header3></Header3>
+      <MainCover></MainCover>
     </>
   )
 }
---
+++ src/Components/Main-Cover/MainCover.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MainCover.css'
+const MainCover = () => {
+  return (
+    <div className='cover-img-wrapper'>
+        <img className='cover-img' src="assets/ashley-cover-pic.png" alt="ashley-cover" />
+    </div>
+  )
+}
+
+export default MainCover

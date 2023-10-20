b42f8de46104bd507fb347a6d4bc2a17422e2b3b
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2023-10-20 12:12:03 +0530

    Main Cover Image Designed
    Added text on the cover photo and also designed cover image for mobile view.

--- src/Components/Main-Cover/MainCover.jsx
+++ src/Components/Main-Cover/MainCover.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import './MainCover.css'
 const MainCover = () => {
   return (
-    <div className='cover-img-wrapper'>
-        <img className='cover-img' src="assets/ashley-cover-pic.png" alt="ashley-cover" />
+    <div className='cover-wrap'>
+      <div className='cover-img-wrapper'>
+          <img className='cover-img' src="assets/ashley-cover-pic.png" alt="ashley-cover" />
+      </div>
+      <div className='cover-content'>
+        <h1>Shop Ashley Furniture with Acima</h1>
+        <h3>Get approved to lease up to $5,000 in merchandise with free delivery*</h3>
+        <a href="" className='app-btn btn-1'>Get the App</a>
+      </div>
     </div>
   )
 }

330a1e918453ebd95515db854ec8b6f93e1d35df
Vivek Vinayak Abhale <109742123+vivekabhale48@users.noreply.github.com>
2023-10-22 00:49:38 +0530

    Footer Design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './Components/Header1/Header'
 import Header3 from './Components/Header3/Header3'
 import MainCover from './Components/Main-Cover/MainCover'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Header></Header>
       <Header3></Header3>
       <MainCover></MainCover>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div class="section-1">
+            <div class="content-1">
+                <div class="download-pack">
+                    <h4>Download our mobile app!</h4>
+                    <div class="images">
+                        <img src="assets/footer/google-play-badge.svg" alt="" />
+                        <img src="assets/footer/image 15.svg" alt="" />
+                    </div>
+                </div>
+            </div>
+            <p class="description">*The advertised service is a rental or lease purchase agreement provided by Acima. It is not a loan, credit or financing. While no credit history is required, Acima obtains information from consumer reporting agencies in connection with a lease application. Acima Cash Price includes a markup over invoice price. Acquiring ownership by leasing costs more than the retailer’s cash price. Not available in MN, NJ, WI, or WY. Merchant participating locations only. Not all applicants are approved. See lease for details. The No Credit Option" or “All Without Credit” means this is not a credit, loan, or financing transaction. It does not mean or imply no inquiry will be made of credit history or creditworthiness. We consider multiple data points in reviewing your application and regularly approve customers with less than perfect credit history.</p>
+        </div>
+        <div class="section-2">
+            <img src="assets/footer/Facebook.svg" alt="" />
+            <img src="assets/footer/Linkedin.svg" alt="" />
+            <img src="assets/footer/Twitter.svg" alt="" />
+            <img src="assets/footer/Twitter Copy.svg" alt="" />
+        </div>
+        <div class="section-3">
+            <div class="div-1">
+                <ul>
+                    <li>How It Works</li>
+                    <li>Ways to Shop</li>
+                    <li>In-Store Retailers</li>
+                    <li>For Partners</li>
+                </ul>
+            </div>
+            <div class="div-1">
+                <ul>
+                    <li>About Us</li>
+                    <li>News</li>
+                    <li>Support/FAQ</li>
+                </ul>
+            </div>
+            <div class="div-1">
+                <ul>
+                    <li>Careers</li>
+                    <li>Invest</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            <div class="div-1">
+                <ul>
+                    <li>Accessibility</li>
+                    <li>Solutions</li>
+                    <li>Digital</li>
+                </ul>
+            </div>
+            <div class="div-1">
+                <ul>
+                    <li>Benefits Plus</li>
+                    <li>Partners Locations</li>
+                    <li>Rent-A-Center</li>
+                </ul>
+            </div>
+        </div>
+        <div class="section-4">
+            <div class="logo">
+                <img src="assets/footer/Color.svg" alt="" />
+                <p>© 2021 Acima, All Rights Reserved</p>
+            </div>
+            <div class="services">
+                <div class="top-ser">
+                    <span>Terms of Services</span>
+                    <span>Privacy Policy</span>
+                    <span>Your California Privacy Rights</span>
+                    <span>Employee Privacy Policy</span>
+                    <span>Your Privacy Choices</span>
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
